94ee9d5d5eebbbe611083fd96d97fac204d2b322
Taylor Wilhite <t.wilhite@amaryllisapparel.com>
2018-05-25 13:31:32 -0700

    add views and connect index page

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.set('view engine', 'ejs');
 
 // Routes
 
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+
 // Start Server
 app.listen(3000, function(){
 	console.log('Server is running on port 3000');
