7629fe685796f52550875498b3eb04dfd8491aa3
Taylor Wilhite <t.wilhite@amaryllisapparel.com>
2018-05-25 13:21:15 -0700

    set up express and listening server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("This is an app working on mac");
+// Set Requirements
+var express = require('express');
+var app = express();
+
+// Config
+app.set('view engine', 'ejs');
+
+// Routes
+
+// Start Server
+app.listen(3000, function(){
+	console.log('Server is running on port 3000');
+});
