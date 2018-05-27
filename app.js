e239f054d2cf077353387fe7a46c60db30267224
Taylor Wilhite <taylorwilhite@gmail.com>
2018-05-27 12:16:23 -0700

    add bodyparser and turn supplier info on form into object

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Set Requirements
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // Config
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Routes
 
@@ -11,6 +13,9 @@ app.get('/', function(req, res){
 	res.render('index');
 });
 
+app.post('/', function(req, res){
+	console.log(req.body);
+});
 
 // Start Server
 app.listen(3000, function(){
