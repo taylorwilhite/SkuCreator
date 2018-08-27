0c2de8f9c5cbafc2c85ca3ae8c1729e0b5209eb5
Taylor Wilhite <taylorwilhite@gmail.com>
2018-08-27 15:45:37 -0700

    add pseudo code for color code Create route

--- routes/colors.js
+++ routes/colors.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var middleware = require('../middleware');
+var Color = require('../models/color');
 
 router.post('/', middleware.isLoggedIn, (req, res) => {
-
+// send color and code to dB
+// if there, give an error
+// if not there, create and send ok to client
 });
