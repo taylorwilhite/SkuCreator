3e5aa8149818e6a25112d04767ed7e41c168f855
Taylor Wilhite <taylorwilhite@gmail.com>
2018-09-04 14:07:39 -0700

    refactor colors.js to appease linter

--- routes/colors.js
+++ routes/colors.js
@@ -1,35 +1,33 @@
-var express = require('express');
-var router = express.Router();
-var passport = require('passport');
-var middleware = require('../middleware');
-var Color = require('../models/color');
+const express = require('express');
+const middleware = require('../middleware');
+const Color = require('../models/color');
+
+const router = express.Router();
 
 router.post('/', middleware.isLoggedIn, (req, res) => {
-	var newColor = req.body.color.toUpperCase();
-	var newColorCode = req.body.colorCode.toUpperCase();
+  const newColor = req.body.color.toUpperCase();
+  const newColorCode = req.body.colorCode.toUpperCase();
 
-	// send color and code to dB
-	Color.findOne({$or: [{color: newColor}, {colorCode: newColorCode}]}, (err, foundColor) => {
-		if(err){
-			console.log(err);
-			res.status(400).send(err);
-		} else {
-			// if there, give an error
-			if(foundColor){
-				res.status(202).send({error: 'This color or code already exists!'});
-			} else {
-				// if not there, create and send ok to client
-				Color.create({color: newColor, colorCode: newColorCode}, (err, newlyCreated) => {
-					if(err){
-						console.log(err);
-						res.status(400).send({error: 'There was a problem creating this color, please try again'});
-					} else {
-						res.status(200).send({success: newlyCreated});
-					}
-				});
-			}
-		}
-	});
+  // send color and code to dB
+  Color.findOne({ $or: [{ color: newColor }, { colorCode: newColorCode }] }, (err, foundColor) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send(err);
+    } else if (foundColor) {
+      // if there, give an error
+      res.status(202).send({ error: 'This color or code already exists!' });
+    } else {
+      // if not there, create and send ok to client
+      Color.create({ color: newColor, colorCode: newColorCode }, (error, newlyCreated) => {
+        if (err) {
+          console.log(error);
+          res.status(400).send({ error: 'There was a problem creating this color, please try again' });
+        } else {
+          res.status(200).send({ success: newlyCreated });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
