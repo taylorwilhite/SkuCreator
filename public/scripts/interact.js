822795991fb14aa4c4ee90db7fe276b8a21b14de
Taylor Wilhite <taylorwilhite@gmail.com>
2018-09-05 08:27:49 -0700

    make eslint happy with interact.js

--- public/scripts/interact.js
+++ public/scripts/interact.js
@@ -1,159 +1,153 @@
-var fieldNum = 2;
-var kimonoStyle = false;
-var regSizes = ['XL', 'L', 'M', 'S'];
-var kimonoSizes = ['LXL', 'SM'];
+/* eslint-env browser */
+let kimonoStyle = false;
+const regSizes = ['XL', 'L', 'M', 'S'];
+const kimonoSizes = ['LXL', 'SM'];
 
-function selectAll(source){
-	checkboxes = document.getElementsByName('size[]');
-	for(var i=0; i<checkboxes.length; i++) {
+function selectAll(source) {
+  const checkboxes = document.getElementsByName('size[]');
+  for (let i = 0; i < checkboxes.length; i += 1) {
     checkboxes[i].checked = source.checked;
   }
-};
+}
 
-function fillColorCode(event){
-	var colorIndex = event.target.name.replace(/\[colorName\]/, '[colorCode]');
-	var foundCode = clientColorList.find(function(selected) {
-		return selected.color === event.target.value
-	}).colorCode;
-	document.querySelector("input[name='" + colorIndex + "']").value = foundCode;
-};
+function fillColorCode(event) {
+  const colorIndex = event.target.name.replace(/\[colorName\]/, '[colorCode]');
+  const foundCode = clientColorList.find(
+    selected => selected.color === event.target.value,
+  ).colorCode;
+  document.querySelector("input[name='" + colorIndex + "']").value = foundCode;
+}
 
-function addColorFields(){
-	var container = document.getElementById('colorFields');
-	var colorDiv = document.createElement('div');
-	var colorField = document.createElement('input');
-	var colorFieldCode = document.createElement('input');
-	var pictureLink = document.createElement('input');
-	var fbColorField = document.createElement('input');
-	var delButton = document.createElement('button');
-	colorField.type = 'text';
-	colorField.name = 'colorSet[color' + fieldNum + '][colorName]';
-	colorField.classList.add('colorName');
-	colorField.placeholder = 'Color';
-	colorField.setAttribute('list', 'colorNameList');
-	colorField.setAttribute('onchange', 'fillColorCode(event)');
-	colorFieldCode.type = 'text';
-	colorFieldCode.name = 'colorSet[color' + fieldNum + '][colorCode]';
-	colorFieldCode.placeholder = 'Color Code';
-	colorFieldCode.setAttribute('readonly', '');
-	pictureLink.type = 'text';
-	pictureLink.name = 'colorSet[color' + fieldNum + '][pictureLink]';
-	pictureLink.placeholder = 'Image Link';
-	fbColorField.type = 'text';
-	fbColorField.name = 'colorSet[color' + fieldNum + '][fbColor]';
-	fbColorField.placeholder = 'FB Color Number';
-	delButton.classList.add('delete-color');
-	delButton.type = 'button';
-	delButton.onclick = removeColorField;
-	delButton.innerHTML = 'Delete';
+function removeColorField() {
+  const colorDiv = this.parentNode;
+  const colorFields = colorDiv.parentNode;
+  colorFields.removeChild(colorDiv);
+}
 
-	
-	colorDiv.appendChild(colorField);
-	colorDiv.appendChild(colorFieldCode);
-	colorDiv.appendChild(pictureLink);
-	colorDiv.appendChild(fbColorField);
-	colorDiv.appendChild(delButton);
-	container.appendChild(colorDiv);
-	fieldNum++;
-};
+function addColorFields() {
+  let fieldNum = 2;
+  const container = document.getElementById('colorFields');
+  const colorDiv = document.createElement('div');
+  const colorField = document.createElement('input');
+  const colorFieldCode = document.createElement('input');
+  const pictureLink = document.createElement('input');
+  const fbColorField = document.createElement('input');
+  const delButton = document.createElement('button');
+  colorField.type = 'text';
+  colorField.name = 'colorSet[color' + fieldNum + '][colorName]';
+  colorField.classList.add('colorName');
+  colorField.placeholder = 'Color';
+  colorField.setAttribute('list', 'colorNameList');
+  colorField.setAttribute('onchange', 'fillColorCode(event)');
+  colorFieldCode.type = 'text';
+  colorFieldCode.name = 'colorSet[color' + fieldNum + '][colorCode]';
+  colorFieldCode.placeholder = 'Color Code';
+  colorFieldCode.setAttribute('readonly', '');
+  pictureLink.type = 'text';
+  pictureLink.name = 'colorSet[color' + fieldNum + '][pictureLink]';
+  pictureLink.placeholder = 'Image Link';
+  fbColorField.type = 'text';
+  fbColorField.name = 'colorSet[color' + fieldNum + '][fbColor]';
+  fbColorField.placeholder = 'FB Color Number';
+  delButton.classList.add('delete-color');
+  delButton.type = 'button';
+  delButton.onclick = removeColorField;
+  delButton.innerHTML = 'Delete';
 
-function removeColorField(){
-	var colorDiv = this.parentNode;
-	var colorFields = colorDiv.parentNode;
-	colorFields.removeChild(colorDiv);
-};
+  colorDiv.appendChild(colorField);
+  colorDiv.appendChild(colorFieldCode);
+  colorDiv.appendChild(pictureLink);
+  colorDiv.appendChild(fbColorField);
+  colorDiv.appendChild(delButton);
+  container.appendChild(colorDiv);
+  fieldNum += 1;
+}
 
-function sendData(url, formId){
-	var form = document.getElementById(formId);
+function makeColor(data) {
+  const container = document.getElementById('main-container');
+  const tempFlash = document.createElement('div');
+  const tempMessage = document.createElement('p');
+  tempFlash.classList.add('alert', 'temp-flash');
 
-	var formData = {}
-	formData.color = form[0].value;
-	formData.colorCode = form[1].value;
+  if (data.success) {
+    // create new color in list
+    clientColorList.push(data.success);
+    const dataList = document.getElementById('colorNameList');
+    const newColorOption = document.createElement('option');
+    newColorOption.value = data.success.color;
+    dataList.appendChild(newColorOption);
 
-	return fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(formData)
-	})
-	.then(response => {return response.json()})
-	.then(data => makeColor(data));
-};
+    // display confirmation
+    tempMessage.innerHTML = 'Option Added Successfully!';
+    tempFlash.classList.add('alert-success');
+  } else {
+    // check for error and display error flash
+    tempMessage.innerHTML = data.error;
+    tempFlash.classList.add('alert-error');
+  }
+  tempFlash.appendChild(tempMessage);
+  container.insertAdjacentElement('afterbegin', tempFlash);
+}
 
-function makeColor(data){
-	var container = document.getElementById('main-container');
-	var tempFlash = document.createElement('div');
-	var tempMessage = document.createElement('p');
-	tempFlash.classList.add('alert', 'temp-flash');
+function sendData(url, formId) {
+  const form = document.getElementById(formId);
 
-	if(data.success){
-		// create new color in list
-		clientColorList.push(data.success);
-		var dataList = document.getElementById('colorNameList');
-		var newColorOption = document.createElement('option');
-		newColorOption.value = data.success.color;
-		dataList.appendChild(newColorOption);
+  const formData = {};
+  formData.color = form[0].value;
+  formData.colorCode = form[1].value;
 
-		// display confirmation
-		tempMessage.innerHTML = 'Option Added Successfully!';
-		tempFlash.classList.add('alert-success');
-	} else {
-		// check for error and display error flash
-		tempMessage.innerHTML = data.error;
-		tempFlash.classList.add('alert-error');
-	}
-	tempFlash.appendChild(tempMessage);
-	container.insertAdjacentElement('afterbegin', tempFlash);
-};
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  })
+    .then(response => response.json())
+    .then(data => makeColor(data));
+}
 
-function kimonoSizeToggle(){
-	if(kimonoStyle == false){
-		regSizes.forEach(function(size){
-			deleteSizes(size);
-		});
-		kimonoSizes.forEach(function(size){
-			addSizes(size);
-		});
-	} else {
-		kimonoSizes.forEach(function(size){
-			deleteSizes(size);
-		});
-		regSizes.forEach(function(size){
-			addSizes(size);
-		});
-	}
-	kimonoStyle = !kimonoStyle;
-};
+function deleteSizes(size) {
+  const deleteSize = document.querySelector('input[value=' + size + ']');
+  const sizeLabel = deleteSize.parentNode;
+  sizeLabel.parentNode.removeChild(sizeLabel);
+}
 
-function deleteSizes(size){
-	var deleteSize = document.querySelector('input[value=' + size + ']');
-	var sizeLabel = deleteSize.parentNode;
-	sizeLabel.parentNode.removeChild(sizeLabel);
-};
+function addSizes(size) {
+  const sizeField = document.getElementById('sizeContainer');
+  const labelContent = document.createTextNode(size);
+  const sizeLabel = document.createElement('label');
+  const sizeCheck = document.createElement('input');
+  sizeCheck.type = 'checkbox';
+  sizeCheck.name = 'size[]';
+  sizeCheck.value = size;
+  sizeLabel.appendChild(labelContent);
+  sizeLabel.prepend(sizeCheck);
+  sizeField.prepend(sizeLabel);
+}
 
-function addSizes(size){
-	var sizeField = document.getElementById('sizeContainer');
-	var labelContent = document.createTextNode(size);
-	var sizeLabel = document.createElement('label');
-	var sizeCheck = document.createElement('input');
-	sizeCheck.type = 'checkbox';
-	sizeCheck.name = 'size[]';
-	sizeCheck.value = size;
-	sizeLabel.appendChild(labelContent);
-	sizeLabel.prepend(sizeCheck);
-	sizeField.prepend(sizeLabel);
-};
+function kimonoSizeToggle() {
+  if (kimonoStyle === false) {
+    regSizes.forEach(size => deleteSizes(size));
+    kimonoSizes.forEach(size => addSizes(size));
+  } else {
+    kimonoSizes.forEach(size => deleteSizes(size));
+    regSizes.forEach(size => addSizes(size));
+  }
+  kimonoStyle = !kimonoStyle;
+}
 
-function validateColors(){
-	var checkedColors = document.getElementsByClassName('colorName');
-	var colorsArr = Array.from(checkedColors)
-	for(i = 0; i < colorsArr.length; i++){
-		console.log(colorsArr[i].value);
-		var colorChecked = clientColorList.find(arrColor => {return arrColor.color.toUpperCase() === colorsArr[i].value.toUpperCase()});
-		if(colorChecked == undefined){
-			alert('One or more Colors is not in the system. Please correct your colors.');
-			return false;
-		};
-	};
-};
+
+function validateColors() {
+  const checkedColors = document.getElementsByClassName('colorName');
+  const colorsArr = Array.from(checkedColors);
+  for (let i = 0; i < colorsArr.length; i += 1) {
+    const colorChecked = clientColorList.find(
+      arrColor => arrColor.color.toUpperCase() === colorsArr[i].value.toUpperCase(),
+    );
+    if (colorChecked === undefined) {
+      alert('One or more Colors is not in the system. Please correct your colors.');
+      return false;
+    }
+  }
+}
