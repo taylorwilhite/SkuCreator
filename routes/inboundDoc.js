d88c92c585a3555394270e5b0d7be46b26a0d7c8
Taylor Wilhite <taylorwilhite@Taylors-Mac.local>
2018-10-03 19:01:27 -0700

    add post route for inbound doc

--- routes/inboundDoc.js
+++ routes/inboundDoc.js
@@ -1,10 +1,46 @@
 const express = require('express');
+const request = require('request');
 const middleware = require('../middleware');
+const routeFunctions = require('../middleware/routeFunctions');
+
+const { makeExcel } = routeFunctions;
 
 const router = express.Router();
 
 router.get('/', middleware.isLoggedIn, (req, res) => {
   res.render('inboundDocs');
+});
+
+router.post('/', middleware.isLoggedIn, (req, res) => {
+  const { inboundSkus } = req.body;
+  const SkuArr = inboundSkus.split('\r\n');
+
+  const reqBody = {
+    ProductSkus: SkuArr,
+    TenantToken: req.session.TenantToken,
+    UserToken: req.session.UserToken,
+  };
+
+  request(
+    {
+      method: 'POST',
+      url: 'https://app.skuvault.com/api/products/getProducts',
+      headers: [{ 'Content-Type': 'application/json', Accept: 'application/json' }],
+      json: true,
+      body: reqBody,
+    }, (err, response, body) => {
+      if (err) {
+        console.log(err);
+        req.flash('error', err);
+        res.redirect('back');
+      } else if (response.statusCode === 200) {
+        makeExcel(body);
+      } else {
+        req.flash('error', `Possible error, unexpected response code: ${response.statusCode}`);
+        res.redirect('back');
+      }
+    },
+  );
 });
 
 module.exports = router;
