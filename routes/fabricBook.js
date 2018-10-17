e567f53f442476657578c47aa947d98e4a8238a6
Taylor Wilhite <taylorwilhite@gmail.com>
2018-10-17 13:05:49 -0700

    remove colors from fabricBook creation

--- routes/fabricBook.js
+++ routes/fabricBook.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const request = require('request');
-const colors = require('colors');
 const middleware = require('../middleware');
 const Material = require('../models/material');
 const routeFunctions = require('../middleware/routeFunctions');
@@ -48,27 +47,25 @@ router.post('/', middleware.isLoggedIn, (req, res) => {
         console.log(err);
         req.flash('error', err);
         res.redirect('back');
-      } else if (response.statusCode === 202) {
+      } else if (response.statusCode === 200) {
+        console.log(`Submitted, response: ${response.statusCode.toString()} ${body.Status}`);
+        req.flash('success', 'SKUs Created successfully!');
+        res.redirect('/skuCreation');
+      } else {
         // logic for error/success flashes
-        console.log('Submitted, response: ' + response.statusCode.toString().yellow + ' ' + body.Status.yellow);
+        console.log(`Submitted, response: ${response.statusCode.toString()} ${body.Status}`);
         const errors = [];
         body.Errors.forEach((error) => {
-          const skuError = error.Sku + ': ' + error.ErrorMessages;
-          console.log('ERROR '.red + skuError); // log the error
-          errors.push(skuError);
+          let newError;
+          if (error.Sku) {
+            newError = `${error.Sku}: ${error.ErrorMessages}`;
+          } else {
+            newError = `${error.ErrorMessages}`;
+          }
+          console.log(`ERROR ${newError}`); // log the error
+          errors.push(newError);
         });
         req.flash('error', errors.join('<br>'));
-        res.redirect('back');
-      } else if (response.statusCode === 200) {
-        console.log('Submitted, response: ' + response.statusCode.toString().green + ' ' + body.Status.green);
-        req.flash('success', 'SKUs Created successfully!');
-        res.redirect('/fabricBooks');
-      } else {
-        console.log('Submitted, response: ' + response.statusCode.toString().red + ' ' + body.Status.red);
-        body.Errors.forEach((error) => {
-          console.log('ERROR '.red + error.Sku + ': ' + error.ErrorMessages);
-        });
-        req.flash('error', 'Possible error, unexpected response code: ' + response.statusCode);
         res.redirect('back');
       }
     },
