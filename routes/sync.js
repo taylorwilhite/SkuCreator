7756a96db61437202fbcfdac75c41332bc72ca6c
Taylor Wilhite <taylorwilhite@gmail.com>
2018-11-16 07:23:18 -0800

    add classifications route

--- routes/sync.js
+++ routes/sync.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const request = require('request');
 const middleware = require('../middleware');
 
 const router = express.Router();
 
 router.get('/', middleware.isLoggedIn, (req, res) => {
   res.render('sync');
+});
+
+router.get('/classifications', middleware.isLoggedIn, (req, res) => {
+  const auth = {
+    TenantToken: req.session.TenantToken,
+    UserToken: req.session.UserToken,
+  };
+
+  request({
+    method: 'POST',
+    url: 'https://app.skuvault.com/api/products/getClassifications',
+    headers: [{ 'Content-Type': 'application/json', Accept: 'application/json' }],
+    json: true,
+    body: auth,
+  }, (err, response, body) => {
+    if (err) {
+      console.log(err);
+      res.status(400).send({ error: { message: err } });
+    } else if (response.statusCode === 200) {
+      // Update database here
+      res.status(200).send({ success: { message: 'Classifications updated successfully!' } });
+    } else {
+      res.status(response.statusCode).send({ error: { message: `Error ${response.statusCode}: ${body.Errors}` } });
+    }
+  });
 });
 
 module.exports = router;
