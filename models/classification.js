036425036e016783ed26e71b853637fad12a627d
Taylor Wilhite <taylorwilhite@gmail.com>
2018-11-20 09:23:18 -0800

    Add attributes to classification Schema

--- models/classification.js
+++ models/classification.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const classificationSchema = new mongoose.Schema({
   name: String,
   isEnabled: Boolean,
+  attributes: [{ name: String, values: [String] }], // This is for later features
 });
 
 module.exports = mongoose.model('Classification', classificationSchema);
