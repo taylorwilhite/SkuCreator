70fd86fd4918f0bbff6d36d315f673e82b917e20
Taylor Wilhite <taylorwilhite@gmail.com>
2020-02-28 16:01:14 -0800

    remove care instructions from sku creation

--- middleware/skuCreation.js
+++ middleware/skuCreation.js
@@ -16,7 +16,7 @@ module.exports = async function skuCreation(body, tenant, user) {
   const sku = body.sku.parent;
   const desc = body.variantTitle;
   const {
-    classification, brand, fbCode, trimCode, hps, inseam, neckType, care,
+    classification, brand, fbCode, trimCode, hps, inseam, neckType,
   } = body;
   const colorSet = Object.entries(body.colorSet);
   const sizes = body.size;
@@ -93,7 +93,6 @@ module.exports = async function skuCreation(body, tenant, user) {
           HPS: hps,
           'Neck type': neckType,
           'Fabric Content': content,
-          'Care Instructions': care,
           'Fabric Construction': construction,
         },
         Classification: classification,
